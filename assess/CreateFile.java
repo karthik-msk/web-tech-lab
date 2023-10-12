f2f55a91ad852ecc55c4593ec3c89de95cc18b06
Karthikeyan M S <125592336+karthik-msk@users.noreply.github.com>
2023-10-12 21:43:26 +0530

    Update CreateFile.java

--- assess/CreateFile.java
+++ assess/CreateFile.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class CreateFile {
     public static void main(String args[]) throws IOException{
         File fileptr = new File("newfile.txt");
-        if(fileptr.createNewFile()){
+        if(fileptr.createNewFile()){ //fileptr.createNewFile() function to create a file
             System.out.println("File with name = "+fileptr.getName()+" has been created");
         }
         else{
