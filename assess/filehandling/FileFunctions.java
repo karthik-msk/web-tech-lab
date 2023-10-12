e55c63ac43e19d73ae6400cea97e8aa04c50da1f
Karthikeyan M S <125592336+karthik-msk@users.noreply.github.com>
2023-10-12 22:03:39 +0530

    Add files via upload

---
+++ assess/filehandling/FileFunctions.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+
+public class FileFunctions { 
+  public static void main(String[] args) throws IOException{
+    File file = new File("sample.txt");
+    if (file.exists()) {
+      System.out.println("File name : " + file.getName());
+      System.out.println("Absolute path : " + file.getAbsolutePath());
+      System.out.println("File size in bytes : " + file.length());
+      System.out.println("Write Permission there ? " + file.canWrite());
+      System.out.println("Read Permission there ? " + file.canRead());
+      System.out.println("Is it a file ? " + file.isFile());
+      System.out.println("Is it a directory ? " + file.isDirectory());
+    } 
+    else {
+      System.out.println("The file does not exist.");
+    }
+  }
+}
